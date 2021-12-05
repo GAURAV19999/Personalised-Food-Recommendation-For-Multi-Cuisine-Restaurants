f927dc1fccee2e8c131ade3dbbdf84c4387bba2b
GAURAV KUMAR VISHWAKARMA <76050264+GAURAV19999@users.noreply.github.com>
2021-12-05 09:00:19 +0530

    Add files via upload

---
+++ dep.py
@@ -0,0 +1,17 @@
+import pandas as pd 
+import streamlit as st
+import warnings
+warnings.filterwarnings("ignore")
+
+def main():
+    df = pd.read_csv(r"C:\Users\GAURAV KUMAR\Desktop\userdata.csv")
+    df.to_csv("userdata.csv")
+    s1 = df.UserID.unique()
+    s11 = st.sidebar.selectbox('User ID',(s1))
+    df1 = df.loc[df["UserID"] == s11]
+    df1 = df1.head(20)
+    df1 = df1[['UserID','Order','OrderType']]
+    return(df1)
+
+s = main()
+st.dataframe(s)
