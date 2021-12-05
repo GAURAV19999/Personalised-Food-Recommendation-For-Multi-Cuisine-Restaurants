09db35dec95b740540647df09bb030526b568bb4
GAURAV KUMAR VISHWAKARMA <76050264+GAURAV19999@users.noreply.github.com>
2021-12-05 09:18:43 +0530

    Update dep.py

--- dep.py
+++ dep.py
@@ -6,13 +6,13 @@ warnings.filterwarnings("ignore")
 def main():
     st.title("Personalised-Food-Recommendation-For-Multi-Cuisine-Restaurants")
     st.subheader("Select a id which you'd like to get the recommdation of food :")
-        df = pd.read_csv("userdata.csv")
-        df.to_csv("userdata.csv")
-        s1 = df.UserID.unique()
-        s11 = st.sidebar.selectbox('User ID',(s1))
-        df1 = df.loc[df["UserID"] == s11]
-        df1 = df1.head(20)
-        df1 = df1[['UserID','Order','OrderType']]
-        return(df1)
+    df = pd.read_csv("userdata.csv")
+    df.to_csv("userdata.csv")
+    s1 = df.UserID.unique()
+    s11 = st.sidebar.selectbox('User ID',(s1))
+    df1 = df.loc[df["UserID"] == s11]
+    df1 = df1.head(20)
+    df1 = df1[['UserID','Order','OrderType']]
+    return(df1)
 s = main()
 st.dataframe(s)
